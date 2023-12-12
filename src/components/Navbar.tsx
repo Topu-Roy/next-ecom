b59568799e8e0df4c845cade53096b0def53bcd7
Topu-Roy <topuroy143333@gmail.com>
2023-12-12 19:23:09 +0600

    Feat: Improved UI for navbar with React-Icons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,44 @@
 import React from 'react'
+import { BsCart2, BsSearch } from "react-icons/bs";
+import { HiOutlineChevronDown } from "react-icons/hi2";
+import { LuUser2 } from 'react-icons/lu'
 
 function Navbar() {
     return (
-        <div className="">
-            <div className="">
-                <div className=""><a href="/" className="">E-commerce</a></div>
+        <div className="flex justify-between items-center px-8 h-16">
+            <div className="w-full flex justify-start items-center space-x-16">
+
+                {/* Logo */}
+                <div className=""><a href="/" className="font-bold text-2xl">E-Commerce</a></div>
+
+                {/* Nav Links: Left */}
                 <nav className="">
-                    <ul>
-                        <li><a className='' href="/shop">Shop</a></li>
-                        <li><a className='' href="/filter">Filters</a></li>
-                        <li><a className='' href="/my-products">My Products</a></li>
+                    <ul className='flex justify-center items-center space-x-16 font-medium'>
+                        <li><a href="/shop">Shop</a></li>
+                        <li><a href="/filter">Filters</a></li>
+                        <li><a href="/my-products">My Products</a></li>
                     </ul>
                 </nav>
             </div>
-            <div className=""></div>
+            <div className="w-full flex justify-end items-center space-x-6">
+                <div className="flex justify-center items-center space-x-6 bg-black/5 rounded-full">
+                    <div className="pl-4"><BsSearch size="20" /></div>
+                    <input type="text"
+                        className='h-10 bg-transparent'
+                    />
+                </div>
+                <div className="flex justify-center items-center space-x-6">
+                    <button className="rounded-full bg-slate-200 h-10 w-10 flex items-center justify-center">
+                        <LuUser2 size="20" />
+                    </button>
+                    <button className="rounded-full bg-slate-200 h-10 w-10 flex items-center justify-center">
+                        <BsCart2 size="20" />
+                    </button>
+                    <button className="rounded-full bg-slate-200 h-10 w-10 flex items-center justify-center">
+                        <HiOutlineChevronDown size="20" />
+                    </button>
+                </div>
+            </div>
         </div>
     )
 }
